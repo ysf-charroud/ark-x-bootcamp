6dad7049e41165c829784c8e2a79d4079c7404ba
youssef <youssef.charroud445@gmail.com>
2025-08-11 14:59:36 +0100

    save

---
+++ Route.js
@@ -0,0 +1,92 @@
+import { createServer } from "http";
+import { parse } from "url";
+export default class Route {
+  #get = [];
+  #post = [];
+  #delete = [];
+  #put = [];
+
+  constructor() {
+    this.server = createServer((req, res) => {
+      const method = req.method.toLowerCase();
+      const paredUrl = parse(req.url, true);
+      const path = paredUrl.pathname;
+
+      let found = false;
+
+      let routes = null;
+      switch (method) {
+        case "get":
+          routes = this.#get;
+          break;
+        case "post":
+          routes = this.#post;
+          break;
+        case "delete":
+          routes = this.#delete;
+          break;
+        case "put":
+          routes = this.#put;
+          break;
+        default:
+          res.writeHead(405, { "Content-Type": "text/plain" });
+          res.end(`Method ${method} not allowed\n`);
+          return;
+      }
+
+      for (const route of routes) {
+        if (route.endpoint === path) {
+          found = true;
+          req.query = paredUrl.query; // Parse query parameters
+          route.callback(req, res);
+          break;
+        }
+      }
+
+      if (!found) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end(`No ${req.method} ${req.url} routes defined\n`);
+      }
+    });
+  }
+  get(endpoint, callback) {
+    if (this.#get.some((route) => route.endpoint === endpoint)) {
+      throw new Error(`Endpoint ${endpoint} already exists for GET method`);
+    }
+    this.#get.push({
+      endpoint,
+      callback,
+    });
+  }
+  post(endpoint, callback) {
+    if (this.#post.some((route) => route.endpoint === endpoint)) {
+      throw new Error(`Endpoint ${endpoint} already exists for GET method`);
+    }
+    this.#post.push({
+      endpoint,
+      callback,
+    });
+  }
+  put(endpoint, callback) {
+    if (this.#put.some((route) => route.endpoint === endpoint)) {
+      throw new Error(`Endpoint ${endpoint} already exists for GET method`);
+    }
+    this.#put.push({
+      endpoint,
+      callback,
+    });
+  }
+  delete(endpoint, callback) {
+    if (this.#delete.some((route) => route.endpoint === endpoint)) {
+      throw new Error(`Endpoint ${endpoint} already exists for GET method`);
+    }
+    this.#delete.push({
+      endpoint,
+      callback,
+    });
+  }
+
+  start(port = 3000, callback) {
+    this.server.listen(port, callback);
+  }
+}
--- challenge.js
+++ challenge.js
@@ -1,54 +1,20 @@
-import { promises as fs } from "fs";
+import Route from "./Route.js";
+import cities from "./cities.js";
+const route = new Route();
+const port = 3000;
+route.get("/weather", (req, res) => {
+  city = req.query.city;
 
-const cities = [
-  { name: "New York", lat: 40.7128, lng: -74.006 },
-  { name: "London", lat: 51.5074, lng: -0.1278 },
-  { name: "Paris", lat: 48.8566, lng: 2.3522 },
-  { name: "Tokyo", lat: 35.6895, lng: 139.6917 },
-  { name: "Sydney", lat: -33.8651, lng: 151.2099 },
-  { name: "Rome", lat: 41.9028, lng: 12.4964 },
-  { name: "Cairo", lat: 30.0444, lng: 31.2357 },
-  { name: "Rio de Janeiro", lat: -22.9068, lng: -43.1729 },
-  { name: "Dubai", lat: 25.2048, lng: 55.2708 },
-  { name: "Rabat", lat: 34.0209, lng: -6.8416 },
-];
-
-// this function used to get city name from input file
-const getFileContent = async (path) => {
-  try {
-    const cityName = await fs.readFile(path, "utf8");
-    return cityName.trim();
-  } catch (err) {
-    console.log(err);
+  // Validate the city query parameter
+  if (!city) {
+    res.writeHead(400, { "Content-Type": "text/plain" });
+    res.end("City query parameter is required\n");
+    return;
   }
-};
 
-const fileExists = async (fileName) => {
-  try {
-    await fs.access(fileName);
-    return true;
-  } catch (error) {
-    return false;
-  }
-};
-const createCityFile = async () => {
-  try {
-    const cityName = await getFileContent("./input.txt");
-    const fileName = `${cityName}.txt`;
-    if (await fileExists(fileName)) {
-      fs.unlink(fileName);
-    }
-    const city = cities.find((item) => item.name === cityName);
-    if (city) {
-      const url = `https://api.open-meteo.com/v1/forecast?latitude=${city.lat}&longitude=${city.lng}&current_weather=true`;
-      const res = await fetch(url);
-      const data = await res.json();
-      const { current_weather: weather } = data;
-      fs.writeFile(fileName, JSON.stringify(weather));
-    } else {
-      console.log("City not found");
-      return;
-    }
-  } catch (error) {}
-};
-createCityFile();
+  res.end(JSON.stringify({ city: req.query.city }));
+});
+
+route.start(port, () => {
+  console.log("Server is running on port " + port);
+});
